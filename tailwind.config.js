632bcceb511ec266c1b52395b9db58f88f8ba382
Tiborz08 <111081518+Tiborz08@users.noreply.github.com>
2022-12-22 18:17:49 +0100

    ajout des fichiers MSP

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'facebook': '#1778f2',
+        'twitter':'#1da1f2',
+        'instagram': '#f00075',
+      },
+    },
+  },
+  plugins: [],
+}
